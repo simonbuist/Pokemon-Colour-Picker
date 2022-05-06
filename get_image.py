dc1de619069547b594f834ef1d1a1a1760b8eb48
simonbuist <89805958+simonbuist@users.noreply.github.com>
2022-05-06 01:01:31 -0400

    v1.1
    Added comments. Apologies for my language on the last update, I have since changed my ways

--- get_image.py
+++ get_image.py
@@ -4,23 +4,30 @@ import requests
 from io import BytesIO
 
 def get_image(number, game, shiny, gender = "f", form = ""):
+    # generate bulbapedia page for the file
     url = get_url(number, game, shiny, gender.lower(), get_form(form))
+    # make request
     response = requests.get(url)
+    # check if request worked
     if response.status_code < 200 or response.status_code > 299:
         print(url)
         return 0
 
+    # get specific file url from bulbapedia html page
     image_url = get_image_url(response.text)
+    # check if request worked
     response = requests.get(image_url)
     if response.status_code < 200 or response.status_code > 299: 
         print(image_url)
         return 1
 
+    # open image response
     img = Image.open(BytesIO(response.content))
 
     return img
 
 
+# map game name to generation code
 def get_gen(game):
     gen_dict = {
         "red":"1b", "blue":"1b", "yellow":"1y",
@@ -33,8 +40,11 @@ def get_gen(game):
         "sword":"8s", "shield":"8s"
     }
 
+    # remove all non-alphanumeric chracters and make lowercase, match to dict entry
     return gen_dict[re.sub(r'[^a-zA-Z0-9]', '', game).lower()]
 
+
+# match form name to form code
 def get_form(form):
     form_dict = {
         "galarian":"G", "alolan":"A", "":""
@@ -43,37 +53,50 @@ def get_form(form):
     return form_dict[form.lower()]
 
 
+# create bulbapedia url
 def get_url(num, gen, shiny, gender, form):
     num_str = str(num)
     
+    # ensure pokemon number has enough 0's ahead of it
     while len(num_str) < 3:
         num_str = "0" + num_str
 
+    # if it's a regional form, add the code
     if form != "":
         num_str = num_str + form
 
+    # if shiny, add shiny tag
     if shiny:
         num_str = num_str + "_s"
 
+    # create the whole url
     url = "https://bulbapedia.bulbagarden.net/wiki/File:Spr_" + get_gen(gen) + "_" + num_str +".png"
 
+    # try requesting the image
     response = requests.head(url)
+
+    # if the request failed, it's a pokemon with gender variants
     if response.status_code < 200 or response.status_code > 299:
-        print(url)
+
+        # reset the number string
         num_str = str(num)
         while len(num_str) < 3:
             num_str = "0" + num_str
 
+        # add f or m for gender
         if shiny:
             num_str = num_str + "_" + gender + "_s"
         else:
             num_str = num_str + "_" + gender
 
+        # recreate the url
         url = "https://bulbapedia.bulbagarden.net/wiki/File:Spr_" + get_gen(gen) + "_" + num_str +".png"
     return url
 
 
+# cut through the html string to find the image url
 def get_image_url(text: str):
+    # i know this isn't the most elegant way to do it, but if it's stupid and it works it's not stupid
     out = text[text.find("<meta property=\"og:image\" content") : text.find("<meta property=\"og:image\" content") + 200]
     out = out[out.find("https://") : out.find(".png") + 4]
     return out
--- main.py
+++ main.py
@@ -3,143 +3,165 @@ from PIL import Image, ImageFont, ImageDraw
 from operator import itemgetter
 from get_image import get_image
 
-def add_margin(pil_img, color):
-    width, height = pil_img.size
-    width_add = round(width * 0.25)
-    height_add = round(height * 0.25)
-    new_width = width + width_add * 2
-    new_height = height + height_add * 2
-    result = Image.new(pil_img.mode, (new_width, new_height), color)
-    result.paste(pil_img, (width_add, height_add))
-    return result
 
-
-def label(img, colour):
-    print(img.size)
-
-    result = Image.new(img.mode, (img.size[0], round(img.size[1] * 1.2)), (255, 255, 255))
-    result.paste(img, (0, 0))
-
-    draw = ImageDraw.Draw(result)
-    font = ImageFont.truetype("arial.ttf", 32)
-    draw.text((img.size[0] / 8, img.size[1] * 1.1 - 16), colour, (0, 0, 0), font=font)
-
-    return result
-
+# get the image ready for processing
 def prepare_image(img):
-    print(img.size)
+    # crop out as much background as possible
     img = img.crop(img.getbbox())
-    # img = img.crop(img.getbbox()).resize((img.size[0] * 4, img.size[1] * 4)).filter(ImageFilter.BoxBlur(1))
+    # stretch it out to 256x256 - distorts a lot of them, but that doesn't change function
     img = img.resize((256, 256))
-    # img = img.convert('RGB').filter(ImageFilter.BoxBlur(1))
+    # convert PNG to PNG with only 256 colours (necessary for extracting colours)
     img = img.convert('P', palette=Image.ADAPTIVE, colors=256)
+    # convert to RGB image (like a JPG)
     return img.convert('RGB')
 
-def choose_colour(img):
-    sensitivity = 0.5
 
+# the actually cool part of the program
+def choose_colour(img):
+    # affects how much saturated colours are prioritized (even if there's a slight larger
+    # grey area, the results "feel" better if colours are prioritized)
+    rgb_difference_boost = 0.5
+
+    # tolerance for including slightly different colours into score of a colour
+    hue_tolerance = 30
+
+    # get all the colours in the image, and how many pixels of each
     colour_list = img.getcolors()
 
-    # remove background (0,0,0 or 255, 255, 255) from colour_list
+    # find the background colour from pixel (0, 0)
     bg_colour = img.getpixel((0, 0))
-    print(bg_colour)    
+    # remove that background colour from the list of colours
     colour_list = list(filter(lambda tup: tup[1] != bg_colour, colour_list))
-    # colour_list = list(filter(lambda tup: tup[1] != (0, 0, 0) and tup[1] != (255, 255, 255), colour_list))
-
+    
     # increase the coverage score of colours with large differences in RGB channels
-    colour_list = [(tup[0] * ((1 - sensitivity/2) + max(
+    # basically check the differences between RGB levels, and increase or decrease
+    # score based on how different (multiply by 1 +- 1/2 boost)
+    colour_list = [(tup[0] * ((1 - rgb_difference_boost/2) + max(
         abs(tup[1][0]-tup[1][1]),
         abs(tup[1][1]-tup[1][2]),
         abs(tup[1][2]-tup[1][0])
-        ) * sensitivity / 255), tup[1]) for tup in colour_list]
+        ) * rgb_difference_boost / 255), tup[1]) for tup in colour_list]
 
-    # print(colour_list)
-
+    # find the 5 most prominent colours
     colour_list.sort(key=itemgetter(0), reverse=True)
     top_five = colour_list[:4]
 
     # add scores of very similar colours to the top 5
     for i in range(0, 4):
         colour = top_five[i]
-        print("Colour: " + str(colour))
-        similar_list = list(filter(lambda tup: abs(colour[1][0] - tup[1][0]) + abs(colour[1][1] - tup[1][1]) + abs(colour[1][2] - tup[1][2]) < 30 and colour != tup, colour_list))
-        print("Similar: " + str(similar_list))
-        # print("Prev Score: " + str(colour[0]))
+        # list all colours with cumulative differences in RBG channels less thatn the hue tolerance
+        similar_list = list(filter(
+            lambda tup: abs(colour[1][0] - tup[1][0]) 
+            + abs(colour[1][1] - tup[1][1]) 
+            + abs(colour[1][2] - tup[1][2]) < hue_tolerance 
+            and colour != tup, colour_list
+            ))
+        # add the scores of similar colours
         top_five[i] = (colour[0] + sum(similar[0] for similar in similar_list), colour[1])
-        # print(" New Score: " + str(top_five[i][0]))
-        # print("\n\n")
 
+    # sort them again with the new scores
     top_five.sort(key=itemgetter(0), reverse=True)
     
-
-    # colour_list_sorted = sorted(colour_list, key=lambda tup: tup[0])
-    # print(colour_list)
+    # return the highest scoring colour
     return top_five[0][1]
 
 
-# img = get_image(20, "sun", True, "f", "alolan")
-def generate_image(num, gen, shiny=False, gender="f", form=""):
-    img = get_image(num, gen, shiny, gender, form)
+# adds a margin to the image in the new colour
+def add_margin(img, color):
+    # get new size
+    width, height = img.size
+    width_add = round(width * 0.25)
+    height_add = round(height * 0.25)
+    new_width = width + width_add * 2
+    new_height = height + height_add * 2
+    # create a new image with the right size and slap the old image in the middle
+    result = Image.new(img.mode, (new_width, new_height), color)
+    result.paste(img, (width_add, height_add))
+    return result
 
-    if img == 0:
-        print("failed\n")
-    else:
-        # img.show()
-        img = prepare_image(img)
-        # img.show()
 
-        # colour_list = img.getcolors()
-        
-        # colour_list = reduce_colours(colour_list)
-        # main_colour = colour_list[0][1]
-
-        main_colour = choose_colour(img)
-        print("#" + str(num))
-        print(main_colour)
-
-        border_img = add_margin(img, main_colour)
-
-        main_colour_name = name_colour(main_colour)
-
-        img_final = label(border_img, main_colour_name)
-        img_final.show()
-        # for color in colour_list:
-        #     print(color)
-
-    print("done!\n\n")
-
-    
+# simple function to find the total difference between 2 RGB values
 def tuple_diff(tup1, tup2):
     return abs(tup1[0] - tup2[0]) + abs(tup1[1] - tup2[1]) + abs(tup1[2] - tup2[2])
 
 
+# finds the nearest colour name to the RGB value
 def name_colour(value):
-    colour_dict = {
-        "red":(255, 0, 0), "orange":(255, 127, 0), "yellow":(255, 255, 0),
-        "green":(0, 255, 0), "blue":(0, 0, 255), "purple":(125, 0, 255),
-        "pink":(255, 0, 255), "brown":(165, 42, 42), "white":(255, 255, 255)
-    }
+    # colour_dict = {
+    #     "red":(255, 0, 0), "orange":(255, 127, 0), "yellow":(255, 255, 0),
+    #     "green":(0, 255, 0), "blue":(0, 0, 255), "purple":(125, 0, 255),
+    #     "pink":(255, 0, 255), "brown":(165, 42, 42), "white":(255, 255, 255)
+    # }
 
+    # dictionary of colour values - arrived on most of them by lowering the contrast
+    # on a rainbow gradient until the greyscale value was the same
     equalized_dict = {
         "red":(255, 74, 57), "orange":(255, 127, 0), "yellow":(155, 138, 0),
         "green":(0, 220, 0), "blue":(110, 110, 255), "purple":(195, 69, 255),
         "pink":(255, 35, 255), "brown":(100, 70, 36), "white":(255, 255, 255)
     }
 
+    # empty dict of matching scores
     match_dict = {}
 
+    # see how close each named colour is to the RGB value
     for colour in equalized_dict.keys():
         match_dict[colour] = tuple_diff(value, equalized_dict[colour])
 
+    # sort by closeness and return closest
     match_list = list(sorted(match_dict.keys(), key=lambda val: match_dict[val]))
-
-    print(match_list[0])
-
     return match_list[0]
 
-    
+
+# adds a label with the colour name to the image - it looks a bit silly but I think that's charming
+def label(img, colour):
+
+    # create a new image with some white space at the bottom and copy old image in
+    result = Image.new(img.mode, (img.size[0], round(img.size[1] * 1.2)), (255, 255, 255))
+    result.paste(img, (0, 0))
+
+    # add text
+    draw = ImageDraw.Draw(result)
+    font = ImageFont.truetype("arial.ttf", 32)
+    draw.text((img.size[0] / 8, img.size[1] * 1.1 - 16), colour, (0, 0, 0), font=font)
+
+    return result
 
 
+
+#main function for now
+def generate_image(num, gen, shiny=False, gender="f", form=""):
+
+    # function to get the image from bulbapedia
+    img = get_image(num, gen, shiny, gender, form)
+
+    # error checks
+    if img == 0 or img == 1:
+        print("failed\n")
+    else:
+        # adjusts the image - see 1st function
+        img = prepare_image(img)
+
+        # chooses the main colour - see 2nd function
+        main_colour = choose_colour(img)
+
+        # adds a margin of the main colour - see 3rd function
+        border_img = add_margin(img, main_colour)
+
+        # gets a name for the colour - see 4th function
+        # this is the bit that currently isn't good - update to come
+        main_colour_name = name_colour(main_colour)
+
+        # adds a label with the colour name - see 5th function
+        img_final = label(border_img, main_colour_name)
+
+        # shows the image
+        img_final.show()
+
+    print("done!\n\n")
+
+
+# to run the program a bunch of times
 # good tests 227, 643, 69, 758, 553, 500, 329, 339, 348
 for i in range(50):
     generate_image(randint(1, 600), "sun", False)
